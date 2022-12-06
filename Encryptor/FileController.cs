658e95d49a243d9df5f5ba90ba2e3e461197ff08
Admin <Admin@DESKTOP-604CIS3>
2022-12-06 18:07:07 +0200

    Add project files.

---
+++ Encryptor/FileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Encryptor
+{
+    internal sealed class FileController
+    {
+        private string path = "Users.json";
+
+        private static FileController _instance;
+
+        private FileController(){ }
+        public static FileController GetInstance() 
+        {
+            if (_instance == null)
+            {
+                _instance = new FileController();
+            }
+            return _instance; 
+        }
+
+        public void WriteInfo(List<User> list) => File.WriteAllText(path, JsonSerializer.Serialize(list));
+        public UserList ReadInfo()
+        {
+            UserList temp = new UserList();
+            if (File.Exists(path))
+            {
+                string tempUsers = File.ReadAllText(path);
+                temp.Users = JsonSerializer.Deserialize<List<User>>(tempUsers);
+            }
+            return temp;
+        }
+    }
+}
