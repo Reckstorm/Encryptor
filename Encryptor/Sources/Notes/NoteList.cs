71f0eacaf6d62cd0c6d6108729f328dc51bfe6ec
Mihail <lolvillian@gmail.com>
2022-12-07 20:49:32 +0200

    Adding Notes Feature

---
+++ Encryptor/Sources/Notes/NoteList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Encryptor.Sources.Notes
+{
+    internal class NoteList : List<Note>
+    {
+        public List<Note> FindPriority(string priority) => FindAll(x => x.Priority.Equals(priority));
+        public List<Note> FindDuplicates(string title) => FindAll(x => x.Title.Equals(title));
+        public int FindIndexByTitle(string title) => FindIndex(x => x.Title.Equals(title));
+        public bool RemoveByTitle(string title)
+        {
+            Note temp = Find(x => x.Title.Equals(title));
+            if (temp != null)
+            {
+                Remove(temp);
+                return true;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string temp = "";
+            foreach (Note item in this)
+            {
+                temp += item + "\n";
+            }
+            return temp;
+        }
+    }
+}
---
+++ Encryptor/Sources/Users/UserList.cs
@@ -0,0 +1,11 @@
+﻿namespace Encryptor.Sources.Users
+{
+    internal class UserList : List<User>, IDisposable
+    {
+        public bool LoginCheck(string login) => this.Any(x => x.Login.Equals(login.ToLower()));
+
+        public User Find(User temp) => this.FirstOrDefault(x => x.Login.Equals(temp.Login.ToLower()));
+
+        public void Dispose() => FileController.GetInstance().WriteInfo(this);
+    }
+}
